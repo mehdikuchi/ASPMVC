ecd5af054e51ae96d665525d1975b04b1fe7e0c3
Mahdi Tanbakuchi <mehdi.kuchi@gmail.com>
2018-07-21 19:39:52 +0430

    The customer Controller added View for Details action is added GetCustomer hardcoded for retrieving customers

---
+++ app1/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using app1.Models;
+
+namespace app1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("Hello");
+        }
+        // GET: Customer
+        public ActionResult Details(int id)
+        {
+            var Customer = GetCustomers().SingleOrDefault(h => h.Id == id);
+            if (Customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Gender=1,Id=1,Name="Mehdi"},
+                new Customer { Gender = 0 , Id = 2, Name = "Narges" }
+            };
+        }
+    }
+}

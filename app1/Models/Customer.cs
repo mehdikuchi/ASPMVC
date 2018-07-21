ecab394e189f38e03ebed140816d2d600be4cdcf
Mahdi Tanbakuchi <mehdi.kuchi@gmail.com>
2018-07-21 18:24:01 +0430

    Data anotation addition of MembershipType Addition of some migrations

--- app1/Models/Customer.cs
+++ app1/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,12 @@ namespace app1.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public int Gender { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MemberShipType MemberShipType { get; set; }// A navigation property    
+        public byte MembershipTypeId { get; set; }//by convetion it will be regarded as for end key by entity framework
     }
 }

4f67e4068f0b9f584816222ec6e61ff45cbc4a7c
Mahdi Tanbakuchi <mehdi.kuchi@gmail.com>
2019-04-29 23:45:49 +0430

    Genre refrence type's migration is added to the movie class plus ome other properties To Do: Populate Genre table using migrations

--- app1/Models/Movie.cs
+++ app1/Models/Movie.cs
@@ -9,6 +9,10 @@ namespace app1.Models
     {
         public int Id { get; set; }
         public String Name { get; set; }
-
+        public Genre Genre { get; set; }
+        public byte GenreId { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public byte InStock { get; set; }
+        public DateTime DateAdded { get; set; }
     }
 }
